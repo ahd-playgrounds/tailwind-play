f581c94908a3fed62846d470f2fd3db218d2a196
Adam Hall <adamhalldesigns@gmail.com>
2022-05-15 11:09:53 +0100

    still playing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
 import { useState } from "react";
+import { loremIpsum } from "lorem-ipsum";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-col bg-cyan-900 text-cyan-100 ">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <p>hello</p>
-        <p>world</p>
-        <button>world</button>
+    <div className="fixed top-0 left-0 h-screen w-screen bg-white text-black">
+      <div className="page-wrapper h-full w-full ">
+        <Nav />
+        <header className="page-header">
+          <h2>Home</h2>
+        </header>
+        <div className="page-content flex flex-col border-x border-grey-200 ">
+          {Array(20)
+            .fill(0)
+            .map((_, id) => (
+              <Tweet key={id} />
+            ))}
+        </div>
+        <div className="page-search">
+          <p>search...</p>
+        </div>
+        <div className="page-side">
+          <p>what&apos;s happening</p>
+        </div>
       </div>
     </div>
+  );
+}
+
+function Tweet() {
+  return (
+    <article className="flex-grow border-b border-grey-200 hover:bg-grey-100">
+      <h2 className="text-lg">{loremIpsum({ count: 4, units: "words" })}</h2>
+      <p>{loremIpsum({ units: "sentences" })}</p>
+    </article>
+  );
+}
+
+function Nav() {
+  return (
+    <nav className="page-nav fixed h-full">
+      <ul className="flex h-full flex-col justify-start gap-3">
+        <li className="text-red-600">Home</li>
+        <li className="text-orange-600">Search</li>
+        <li>Messages</li>
+        <li>Post</li>
+        <li className="flex flex-grow flex-col justify-end">Profile</li>
+      </ul>
+    </nav>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{tsx,jsx}", "./index.html"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: colors.slate["50"],
+      black: colors.slate["800"],
+      grey: colors.slate,
+      red: colors.rose,
+      orange: colors.amber,
+      blue: colors.teal,
+    },
   },
   plugins: [],
 };
